c4eae862774217ef45ec5c20f544db49a9a27c9f
陈燕翔 <22371105>
2024-07-06 19:33:51 +0800

    merge-1

--- software cup/src/router/index.ts
+++ software cup/src/router/index.ts
@@ -1,9 +1,11 @@
-import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory} from 'vue-router'
 import TiptapEditor from '../views/Editor.vue'
+import path = require('path');
+import TheWelcome from '@/components/TheWelcome.vue';
 
 const routes = [
   { path: '/edit/:documentid', component: TiptapEditor},
-  
+  { path: '/', component: TheWelcome},
 ];
 
 const router = createRouter({
