a8e9f4b1d9116af2c471532c28c9ea2038d8ba83
梁坤 <20101021@buaa.edu.cn>
2024-07-19 11:41:16 +0800

    logo

--- software cup/src/router/index.ts
+++ software cup/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHashHistory} from 'vue-router'
-// import TiptapEditor from '../views/Editor.vue'
 import TiptapEditor3 from '../views/Edit/index.vue'
 // import path = require('path');
 import HomeView from '@/views/HomeView.vue';
@@ -7,7 +6,6 @@ import { defineStore} from 'pinia'
 import { h, ref, type Component } from 'vue'
 
 const routes = [
-  // { path: '/edit2/:documentid', component: TiptapEditor},
   { path: '/edit3/:documentid', component: TiptapEditor3, props: true},
   { path: '/', component: HomeView},
 ];
